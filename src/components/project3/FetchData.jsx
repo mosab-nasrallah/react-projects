9b8af24e2752ca55ec41688a549aee28840c9649
Mosab991 <mosabnasrallah@gmail.com>
2025-09-27 00:54:10 +0300

    fetching and displaying a specific meal data, with handling loading and errors

---
+++ src/components/project3/FetchData.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { useAxios } from "./useAxios";
+
+const FetchData = ({ id }) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const { data, loading, error } = useAxios(url);
+  const [meal, setMeal] = useState({});
+
+  const fetchedData = data ? data.meals[0] : null;
+
+  useEffect(() => {
+    if (!fetchedData) return;
+    const {
+      idMeal,
+      strMeal,
+      strCategory,
+      strMealThumb,
+      strInstructions,
+      strYoutube,
+      strTags,
+    } = fetchedData;
+
+    const ingredientsList = [];
+    const measuresList = [];
+
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = fetchedData[`strIngredient${i}`];
+      const measure = fetchedData[`strMeasure${i}`];
+
+      if (ingredient && ingredient.trim()) {
+        ingredientsList.push(ingredient);
+        measuresList.push(measure.trim() ? measure : "");
+      } else {
+        break;
+      }
+    }
+
+    // const tagsList = [];
+    // Object.keys(fetchedData).forEach((key) => {
+    //   if (key.startsWith("strIngredient") && fetchedData[key]) {
+    //     ingredientsList.push(fetchedData[key]);
+    //   }
+    //   if (key.startsWith("strMeasure") && fetchedData[key]) {
+    //     measuresList.push(fetchedData[key]);
+    //   }
+    // if (key === "strTags" && fetchedData["strTags"]) {
+    //   tagsList.push(...fetchedData["strTags"].split(","));} ---> the one under is better and more efficient
+    // });
+    const tagsList = strTags
+      ? strTags
+          .split(",")
+          .map((tag) => tag.trim())
+          .filter((tag) => tag.length > 0)
+      : [];
+    const meal = {
+      mealId: idMeal,
+      mealName: strMeal,
+      category: strCategory,
+      ingredients: ingredientsList,
+      measurements: measuresList,
+      thumbnail: strMealThumb,
+      instructions: strInstructions,
+      Youtube: strYoutube,
+      tags: tagsList,
+    };
+    console.log(meal);
+
+    setMeal(meal);
+  }, [fetchedData]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <h1 style={{ display: "inline" }}>{meal.mealName}</h1>
+      <h1 style={{ display: "inline", marginLeft: "50px" }}>{meal.category}</h1>
+      <h1>
+        <img src={meal.thumbnail} alt={meal.mealName} />
+      </h1>
+    </div>
+  );
+};
+
+export default FetchData;
--- src/components/project3/Project3.jsx
+++ src/components/project3/Project3.jsx
@@ -1,39 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useAxios } from "./useAxios";
+import React from "react";
+import FetchData from "./fetchData";
 
 const Project3 = () => {
-  const url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
-  const { data, loading, error } = useAxios(url);
-  const [ingredients, setIngredients] = useState([]);
-  const [measures, setMeasures] = useState([]);
-  
-
-  const meal = data ? data.meals[0] : null;
-
-  useEffect(() => {
-    if (!meal) return;
-    const ingredientsList = [];
-    const measuresList = [];
-    Object.keys(meal).forEach((key) => {
-      if (key.startsWith("strIngredient") && meal[key]) {
-        ingredientsList.push(meal[key]);
-      }
-      if (key.startsWith("strMeasure") && meal[key]) {
-        measuresList.push(meal[key]);
-      }
-    });
-
-    setIngredients(ingredientsList);
-    setMeasures(measuresList);
-
-  }, [meal]);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-  console.log(ingredients);
-  console.log(measures);
-
-  return <div>Hi</div>;
+  const id = 52772;
+  return (
+    <div>
+      <FetchData id={id} />
+    </div>
+  );
 };
 
 export default Project3;
